468095dede4147f093a21ee64ecc028b4497cd0d
omiralles03 <oupman.miralles@qmr.cat>
2025-03-03 17:30:49 +0100

    ignore files and subdirs + output file

--- Part2/comparator.sh
+++ Part2/comparator.sh
@@ -4,18 +4,41 @@
 # PART 2: Ampliació de funcionalitats
 # -------------------------------------
 
-# TODO: 
-#   1. Comparacio recursiva
-#   2. Comparacio avancada de fitxers
-#   3. Ignorar certs fitxers
-#   4. Comprovacio de permisos
-#   5. Registre en un fitxers
-
 DEBUG=false
+check_perms=false
+
+# Parse getopt arguments
+while getopts "f:d:po:" opt; do 
+    case "$opt" in 
+        f)
+            ignore_file="$OPTARG"
+            ;;
+        d)
+            ignore_dir="$OPTARG"
+            ;;
+        p)
+            check_perms=true
+            ;;
+        o)
+            output_file="$OPTARG"
+            ;;
+        *)
+            echo "Invalid option: -${OPTARG}" >&2 
+            exit 1 
+            ;;
+    esac
+done
+shift $((OPTIND-1)) # Remove opts args to access positional args
+
+# Redirect output to a file
+if [ -n "output_file" ]; then
+    exec > "$output_file" 2>&1
+fi
 
 # Comprovar si es passen 2 params i existeixen
 if [ "$#" -ne 2 ]; then
-    echo "Ús: $0 <directori1> <directori2>" >&2
+    echo "Ús: $0 [-f extensio1,extensio2,...] [-d subdirectori] <directori1> <directori2>" >&2
+        
     exit 1
 fi
 DIR1=$1
@@ -41,7 +64,7 @@ advanced_comparison() {
     local file1="$1"
     local file2="$2"
 
-    echo -e "$file1 <--> $file2\n"
+    echo -e "   $file1 <--> $file2\n"
 
     # Get the diff between the two files
         # -B: Ignore blank lines
@@ -72,13 +95,27 @@ advanced_comparison() {
         similarity=100 # For empty files
     fi
 
-    echo -e "Els fitxers tenen un $similarity% de similitud.\n"
+    echo -e "   Els fitxers tenen un $similarity% de similitud.\n"
     if [ "$similarity" -ge 90 ]; then
         # Print the absolute path of the files
         echo "   > $(realpath -e "$file1")"
         echo "   > $(realpath -e "$file2")"
-        echo -e "\nDiferencies:\n"
+        echo -e "\n   Diferencies:\n"
         echo "$diffed"
+    fi
+
+    # Check the permissions of the files
+    if $check_perms; then
+        local perms1=$(stat -c "%a" "$file1")
+        local perms2=$(stat -c "%a" "$file2")
+        if [ "$perms1" != "$perms2" ]; then
+            echo -e "\n   Els permisos son diferents:"
+            echo "      > $(realpath -e "$file1"): $perms1"
+            echo "      > $(realpath -e "$file2"): $perms2"
+        else
+            echo -e "\n   Els permisos son iguals:"
+            echo "      > $perms1"
+        fi
     fi
 
     if $DEBUG; then
@@ -88,19 +125,78 @@ advanced_comparison() {
         echo "Total lines compared: $total_lines"
         echo "Different lines: $diff_lines"
         echo "Similitud: $similarity%"
+        echo "Permisos de $file1: $perms1"
+        echo "Permisos de $file2: $perms2"
     fi
 }
 
-# Iterate over all files in DIR1 and compare them with DIR2
-echo -e "\nComparacio avancada de fitxers:\n"
+echo -e "\nComparacio avancada de fitxers ignorant {$ignore_file} & {$ignore_dir}\n"
+
+# 3. Ignore certain files
+# Convert the file extension string into an array
+if [ -n "$ignore_file" ]; then 
+    IFS=',' read -r -a extensions <<< "$ignore_file"
+fi
+isCompared=false
+# Apply filters to the files
 find "$DIR1" -type f -printf "%P\n" | while IFS= read -r relpath; do
     if [ -f "$DIR2/$relpath" ]; then
-        if ! diff -q "$DIR1/$relpath" "$DIR2/$relpath" >/dev/null; then
-            if $DEBUG; then 
-                advanced_comparison "d1/countries.txt" "d2/countries.txt" 
-            else
-                advanced_comparison "$DIR1/$relpath" "$DIR2/$relpath"
+        skip=false
+
+        # Check if the subdirectory is in the ignore list
+        if [ -n "$ignore_dir" ]; then
+            if [[ "$relpath" == "$ignore_dir"* ]]; then
+                skip=true
+            fi
+        fi
+
+        # Check if the file extension is in the ignore list
+        if [ -n "$ignore_file" ]; then
+            for ext in "${extensions[@]}"; do
+                if [[ "$relpath" == *"$ext" ]]; then
+                    skip=true
+                    break
+                fi
+            done
+        fi
+        # Compare the files if they are not skipped
+        if ! $skip; then 
+            if ! diff -q "$DIR1/$relpath" "$DIR2/$relpath" >/dev/null; then
+                if $DEBUG; then 
+                    advanced_comparison "d1/countries.txt" "d2/countries.txt" 
+                    echo "skip: $skip" 
+                    echo "compared: $isCompared"
+                else
+                    advanced_comparison "$DIR1/$relpath" "$DIR2/$relpath"
+                fi
+                isCompared=true
             fi
         fi
     fi
 done
+
+
+# Open output file
+# dev/tty is used to read input from the terminal
+if [ -n "$output_file" ]; then
+    echo "Resultats guardats a $output_file" > /dev/tty
+    echo "Vols veure els resultats? (y/n)" > /dev/tty
+    read -r res < /dev/tty
+    if [ "$res" == "y" ]; then
+        echo "Com el vols veure?"> /dev/tty
+        echo "vim[1], neovim[2], nano[3], cat[4]: " > /dev/tty
+        read -r option < /dev/tty
+        case $option in
+            1) editor="vim";;
+            2) editor="nvim";;
+            3) editor="nano";;
+            4) editor="cat";;
+            *) 
+                echo "Opció no vàlida. S'obrirà amb cat." > /dev/tty
+                editor="cat"
+                ;;
+        esac
+        echo "Obrint amb $editor..." > /dev/tty
+        $editor $output_file > /dev/tty
+    fi
+fi
