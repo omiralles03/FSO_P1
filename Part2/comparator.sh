cc0cce49f2c1b9d8671c4f2c01299ebb12b5e821
omiralles03 <oupman.miralles@qmr.cat>
2025-03-06 23:02:52 +0100

    refactor getopts, adv_cmp, perms_cmp, calc_sim

--- Part2/comparator.sh
+++ Part2/comparator.sh
@@ -5,23 +5,24 @@
 # -------------------------------------
 
 # Default values
-DEBUG=false
 show_compare=false
-ignore_spaces=false
+ignore_whites=false
 sim=false
-ignore_file=""
+extensions=()
+#ignore_file=""
 ignore_dir=""
 check_perms=false
 output_file=""
 editor="cat"
+
 # Parse getopt arguments
-while getopts "cisf:d:po:" opt; do 
+while getopts "cwsf:d:po:" opt; do 
     case "$opt" in 
         c)
             show_compare=true
             ;;
-        i)
-            ignore_spaces=true
+        w)
+            ignore_whites=true
             ;;
         s)
             sim=true
@@ -36,7 +37,9 @@ while getopts "cisf:d:po:" opt; do
             check_perms=true
             ;;
         o)
+            # Redirect output to the specified file
             output_file="$OPTARG"
+            exec > "$output_file" 2>&1
             ;;
         *)
             echo "Invalid option: -${OPTARG}" >&2 
@@ -45,11 +48,6 @@ while getopts "cisf:d:po:" opt; do
     esac
 done
 shift $((OPTIND-1)) # Remove opts args to access positional args
-
-# Redirect output to a file
-if [ -n "$output_file" ]; then
-    exec > "$output_file" 2>&1
-fi
 
 # Comprovar si es passen 2 params i existeixen
 if [ "$#" -ne 2 ]; then
@@ -64,104 +62,125 @@ if [ ! -d "$DIR1" ] || [ ! -d "$DIR2" ]; then
     exit 1
 fi
 
+echo "-------------------------------------"
+echo "      RESULTATS DE LA COMPARACIO     "
+echo "-------------------------------------"
+echo ""
 # 1. Recursive comparison
 # FLAGS:
 #   -1: Suppress lines unique to file1.
 #   -2: Suppress lines unique to file2.
 #   -3: Suppress lines common to both files.
 echo -e "Fitxers només a $DIR1:\n"
-comm -23 <(find "$DIR1" -type f -printf "%P\n" | sort) <(find "$DIR2" -type f -printf "%P\n" | sort) | sed "s|^|   > |"
+comm -23 <(find "$DIR1" -type f -printf "%P\n" | sort) <(find "$DIR2" -type f -printf "%P\n" | sort) | sed "s|^|\t> |"
 
 echo -e "\nFitxers només a $DIR2:\n"
-comm -13 <(find "$DIR1" -type f -printf "%P\n" | sort) <(find "$DIR2" -type f -printf "%P\n" | sort) | sed "s|^|   > |"
+comm -13 <(find "$DIR1" -type f -printf "%P\n" | sort) <(find "$DIR2" -type f -printf "%P\n" | sort) | sed "s|^|\t> |"
+
+# 2.3 Calculate similarity
+calculate_similarity() {
+    local file1="$1"
+    local file2="$2"
+
+    # Save the content of the files
+    local content1=$(<"$file1")
+    local content2=$(<"$file2")
+
+    # Remove whitespaces if the flag is set
+    if $ignore_whites; then
+        content1=$(echo "$content1" | tr -d '[:space:]')
+        content2=$(echo "$content2" | tr -d '[:space:]')
+    fi
+    
+    # Total number of characters in each file
+    local chars1=${#content1}
+    local chars2=${#content2}
+    local total_chars=$((chars1 > chars2 ? chars1 : chars2))
+    
+    # Number of matching characters
+    local match_chars=0 
+    for ((i = 0; i < total_chars; i++)); do
+        chars1=${content1:$i:1}
+        chars2=${content2:$i:1}
+        if [ "$chars1" == "$chars2" ]; then
+            ((match_chars++))
+        fi
+    done
+    
+    # Calculate the similarity
+    local similarity=0
+    if ((total_chars > 0)); then
+        similarity=$((match_chars * 100 / total_chars))
+    fi 
+    echo "$similarity"
+}
 
 # 2. Advanced comparison
 advanced_comparison() {
     local file1="$1"
     local file2="$2"
 
-    # Get the diff between the two files
-        # -B: Ignore blank lines
-        # -w: Ignore white space when comparing lines
-        # -u: Unified output format
-    if $ignore_spaces; then
-        local diffed=$(diff -u "$file1" "$file2")
+    # Get the total number of lines in each file
+    if $ignore_whites; then
+        local diffed=$(diff -B -w -u0 "$file1" "$file2" | grep -v '^@@')
     else
-        local diffed=$(diff -B -w -u "$file1" "$file2")
+        local diffed=$(diff -u0 "$file1" "$file2" | grep -v '^@@')
+    fi
+    
+    # If flag is set and files are not the same, show the differences
+    if $show_compare && [[ -n "$diffed" ]]; then
+        echo -e "\nLinies diferents:\n" | sed "s|^|\t|"
+        echo "$diffed" | sed "s|^|\t\t|"
+        echo ""
     fi
 
+    # Return the absolute path if the similarity is greater than 90%
     if $sim; then
-        # Get non-empty lines in each file
-            # -c: Count the number of lines
-            # -v: Invert the sense of comparisons (only selects lines that are different)
-            # -e: Regular expression (start of line, any number of spaces, end of line)
-        local total1=$(grep -cve '^[[:space:]]*$' "$file1")
-        local total2=$(grep -cve '^[[:space:]]*$' "$file2")
-        local total_lines=$(( total1 > total2 ? total1 : total2 ))
-    
-        # Count lines that are different
-            # Get all lines that start with + or - (added or removed lines)
-            # Remove lines that start with --- or +++ (file names)
-        local diff_lines=$(echo "$diffed" | grep -E '^[+-]' | grep -vE '^(---|\+\+\+)' | wc -l)
-        diff_lines=$(( (diff_lines % 2) > 0 ? diff_lines / 2 + 1 : diff_lines / 2 ))
-    
-        # Return the absolute name of files with over 90% similarity
-        local similarity
-    
-        if [ "$total_lines" -gt 0 ]; then
-            similarity=$((100 - (diff_lines * 100 / total_lines))) 
-        else
-            similarity=100 # For empty files
-        fi
-    
-        
-        echo -e "      : Els fitxers tenen un $similarity% de similitud.\n"
+        local similarity=$(calculate_similarity "$file1" "$file2")
+        echo -e ": Els fitxers tenen un $similarity% de similitud.\n" | sed "s|^|\t\t|"
         if [ "$similarity" -ge 90 ]; then
             # Print the absolute path of the files
-            echo "      > $(realpath -e "$file1")"
-            echo "      > $(realpath -e "$file2")"
-        fi
-    fi
-    if $show_compare; then
-        echo -e "\n   Linies diferents:\n"
-        echo "$diffed"
-    fi
-    # Check the permissions of the files
-    if $check_perms; then
-        local perms1=$(stat -c "%a" "$file1")
-        local perms2=$(stat -c "%a" "$file2")
-        if [ "$perms1" != "$perms2" ]; then
-            echo -e "\n      - Els permisos son diferents:"
-            echo "         > $(realpath -e "$file1")"
-            echo "            : $perms1 = $(stat -c "%A" "$file1")"
-            echo "         > $(realpath -e "$file2"): $perms2"
-            echo "            : $perms2 = $(stat -c "%A" "$file2")"
-        else
-            echo -e "\n      - Els permisos son iguals:"
-            echo "         > $perms1 = $(stat -c "%A" "$file1")"
+            echo "> $(realpath -e "$file1")" | sed "s|^|\t\t|"
+            echo "> $(realpath -e "$file2")" | sed "s|^|\t\t|"
         fi
     fi
 
-    if $DEBUG; then
-        echo -e "\nDEBUG INFO:"
-        echo "Total lines in $file1: $total1"
-        echo "Total lines in $file2: $total2"
-        echo "Total lines compared: $total_lines"
-        echo "Different lines: $diff_lines"
-        echo "Similitud: $similarity%"
-        echo "Permisos de $file1: $perms1"
-        echo "Permisos de $file2: $perms2"
+    # Print the result
+    if [ -z "$diffed" ]; then
+        echo "* Els fitxers $relpath son iguals." | sed "s|^|\t|"
+    else
+        echo "! Els fitxers $relpath son diferents." | sed "s|^|\t|"
     fi
 }
 
-echo -e "\nComparacio avancada de fitxers ignorant {$ignore_file}\n"
+perms_comparison() {
+    local file1="$1"
+    local file2="$2"
+
+    # Check the permissions of the files
+    local perms1=$(stat -c "%a" "$file1")
+    local perms2=$(stat -c "%a" "$file2")
+    if [ "$perms1" != "$perms2" ]; then
+        echo -e "! Els permisos son diferents:\n" | sed "s|^|\t|"
+        echo "> "$file1" : $perms1 = $(stat -c "%A" "$file1")" | sed "s|^|\t\t|"
+        echo "> "$file2" : $perms2 = $(stat -c "%A" "$file2")" | sed "s|^|\t\t|"
+    else
+        echo -e "* Els permisos son iguals:\n" | sed "s|^|\t|"
+        echo "> "$file1" : $perms1 = $(stat -c "%A" "$file1")" | sed "s|^|\t\t|"
+        echo ""
+    fi
+}
 
 # 3. Ignore certain files
-# Convert the file extension string into an array
-if [ -n "$ignore_file" ]; then 
+
+# Convert the comma-separated string into an array
+if [ -n "$ignore_file" ]; then
     IFS=',' read -r -a extensions <<< "$ignore_file"
+    echo -e "\nComparacio avancada de fitxers ignorant (${extensions[@]}):\n"
+else
+    echo -e "\nComparacio avancada de fitxers:\n"
 fi
-isCompared=false
+
 # Apply filters to the files
 find "$DIR1" -type f -printf "%P\n" | while IFS= read -r relpath; do
     if [ -f "$DIR2/$relpath" ]; then
@@ -186,17 +205,16 @@ find "$DIR1" -type f -printf "%P\n" | while IFS= read -r relpath; do
         # Compare the files if they are not skipped
         if ! $skip; then 
             echo ""
-            echo -e "   Comparant: $DIR1/$relpath <--> $DIR2/$relpath\n"
+            echo -e " || Comparant: $DIR1/$relpath <--> $DIR2/$relpath\n"
             if ! diff -q "$DIR1/$relpath" "$DIR2/$relpath" >/dev/null; then
                 advanced_comparison "$DIR1/$relpath" "$DIR2/$relpath"
-                if $DEBUG; then 
-                    echo "skip: $skip" 
-                    echo "compared: $isCompared"
-                fi
-                isCompared=true
             else
-                echo "      ! Els fitxers $relpath són iguals."
+                echo "* Els fitxers $relpath son iguals." | sed "s|^|\t|"
             fi
+        fi
+
+        if $check_perms; then
+            perms_comparison "$DIR1/$relpath" "$DIR2/$relpath"
         fi
     fi
 done
